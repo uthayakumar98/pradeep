1919aaec5fc417ad7d42f66ee5e985795e5e7c72
uthayakumar98 <51090984+uthayakumar98@users.noreply.github.com>
2019-05-27 22:06:20 +0530

    Update udhai.py

--- udhai.py
+++ udhai.py
@@ -1,7 +1,9 @@
+def posorneg(n):
+  if n>0:
+    return("Positive")
+  elif n<0:
+    return("Negative")
+  else:
+    return("Zero")
 n=int(input())
-if n>0:
-  print("Positive")
-elif n<0:
-  print("Negative")
-else:
-  print("Zero")
+print(posorneg(n))
