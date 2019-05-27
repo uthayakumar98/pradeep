74b20ca51a0ab8c2dad43d7b9092c496915e079b
uthayakumar98 <51090984+uthayakumar98@users.noreply.github.com>
2019-05-27 23:14:09 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,9 +1,10 @@
 a=input()
 a.lower()
 b=['a','e','i','o','u']
+c="abcdefghijklmnopqrstuvwxyz"
 if a in b:
   print("Vowel")
-elif a not in b:
+elif a in c:
   print("Consonant")
 else:
   print("Invalid")
